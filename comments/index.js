d476e538609376ea0399207a9bdbc4f0cb275813
Bonaventura Fleischmann <dev+boni@a2-access.com>
2020-07-31 08:39:04 +0200

    Start using on all backend services the cluster ip service

--- comments/index.js
+++ comments/index.js
@@ -35,7 +35,7 @@ app.post('/posts/:id/comments', async (req, res) => {
   comments.push({ id: commentId, content, status })
   commentsByPostId[req.params.id] = comments;
 
-  await axios.post('http://localhost:4005/events', {
+  await axios.post('http://event-bus-srv:4005/events', {
     type: "CommentCreated",
     data: { id: commentId,
             content,
@@ -62,7 +62,7 @@ app.post('/events', async (req, res) => {
 
     comment.status = status;
 
-    await axios.post('http://localhost:4005/events', {
+    await axios.post('http://event-bus-srv:4005/events', {
       type: 'CommentUpdated',
       data: {
         id,
